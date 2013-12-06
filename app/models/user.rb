4f0344015c624f8443d9ce5b96f93c02ad9c6d1f
Dan Tao <daniel.tao@gmail.com>
2013-12-05 18:13:57 -0800

    enforce user name uniqueness at the model level

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :comments
 
   validates_presence_of   :name
+  validates_uniqueness_of :name
   validates_presence_of   :email
   validates_uniqueness_of :email
 
