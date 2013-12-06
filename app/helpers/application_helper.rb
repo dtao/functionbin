01dcc00dcfcd44d08c382fa8ab3adf49537d034d
Dan Tao <daniel.tao@gmail.com>
2013-12-05 18:13:57 -0800

    working on refactoring w/ some reusable helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,19 @@ module ApplicationHelper
     code_mirror(field_name, value, options.merge(:'data-readonly' => 'nocursor'))
   end
 
+  def link_to_record(record, text=record.name)
+    link_to(text, record)
+  end
+
+  def action_link(text, path, options={})
+    link_to(text, path, options_with_class(options, 'action'))
+  end
+
   def disabled_link(text, options={})
-    class_list = [*options[:class], 'disabled-link'].join(' ')
-    link_to(text, 'javascript:void(0);', options.merge(:class => class_list))
+    link_to(text, 'javascript:void(0);', options_with_class(options, 'disabled-link'))
+  end
+
+  def options_with_class(options, class_name)
+    options.merge(:class => [*options[:class], class_name].join(' '))
   end
 end
