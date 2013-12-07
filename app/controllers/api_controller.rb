bb700de8128b302210d56ee33ec66ac254b6a85a
Dan Tao <daniel.tao@gmail.com>
2013-12-06 17:01:55 -0800

    simplified API controller + added browser format

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,20 +1,20 @@
 class ApiController < ApplicationController
-  # Expose functions Node-style
-  def node
-    function_ids = params[:ids].split(',')
+  def index
+    function_names = params[:functions].split('+')
+
+    # Find all functions with the specified names
+    function_ids = Function.where(:name => function_names).map(&:id)
 
     # Find all implementations for the specified functions
     # (This should be quick; I wouldn't expect there to be tons of implementations
     # for any given function.)
-    @implementations = Implementation.where(:function_id => function_ids).all(:include => :function)
+    implementations = Implementation.where(:function_id => function_ids).all(:include => :function)
 
     # Pick the top-rated implementation for each function
-    @implementations = @implementations.group_by(&:function_id).map do |function_id, impls|
+    @implementations = implementations.group_by(&:function_id).map do |function_id, impls|
       impls.sort_by(&:score).last
     end
 
-    respond_to do |format|
-      format.js { render(:action => 'node') }
-    end
+    render(:action => "#{params[:format] || 'browser'}.js", :'content-type' => 'text/javascript')
   end
 end
